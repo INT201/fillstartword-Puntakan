36771e136ed6242f10102bfdcdfb88b0beca9c26
Puntakan <puntakan.c@mail.kmutt.ac.th>
2022-09-06 01:39:01 +0700

    done

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,14 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
-}
+  if (word.includes(startWord)){
+    return word
+  }
+  else if (word === null | word === undefined){
+    return undefined
+  }
+  else {
+    return startWord + word
+  }
+} 
 module.exports = fillStartWord
